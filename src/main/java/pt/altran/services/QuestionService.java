98068645a664735769854c569f156b987857aa9a
miguel <>
2013-12-13 12:25:30 +0000

    next question

--- src/main/java/pt/altran/services/QuestionService.java
+++ src/main/java/pt/altran/services/QuestionService.java
@@ -6,19 +6,30 @@
 
 package pt.altran.services;
 
-import javax.ejb.Stateless;
+import javax.ejb.Stateful;
+import javax.inject.Inject;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import pt.altran.entities.Answer;
 import pt.altran.entities.Question;
 
 /**
  *
  * @author User
  */
-@Stateless
+@WebService
+@Stateful
 public class QuestionService extends AbstractService<Question> implements QuestionServiceLocal {
     @PersistenceContext(unitName = "webServiceTrivia")
     private EntityManager em;
+    
+    private Question currentQuestion;
+    private Answer currentAnswer;
+    
+    @Inject
+    QuestionaryService questionaryService;
 
     @Override
     protected EntityManager getEntityManager() {
@@ -28,5 +39,20 @@ public class QuestionService extends AbstractService<Question> implements Questi
     public QuestionService() {
         super(Question.class);
     }
+
+    @WebMethod
+    public Question getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(Question currentQuestion) {
+        this.currentQuestion = currentQuestion;
+    }
+
+    @WebMethod
+    public void setCurrentAnswer(Answer currentAnswer) {
+        this.currentAnswer = currentAnswer;
+    }
+    
     
 }
--- src/main/java/pt/altran/services/QuestionaryService.java
+++ src/main/java/pt/altran/services/QuestionaryService.java
@@ -6,19 +6,31 @@
 
 package pt.altran.services;
 
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import pt.altran.entities.Question;
 import pt.altran.entities.Questionary;
 
 /**
  *
  * @author User
  */
+
+@WebService
 @Stateless
 public class QuestionaryService extends AbstractService<Questionary> implements QuestionaryServiceLocal {
     @PersistenceContext(unitName = "webServiceTrivia")
     private EntityManager em;
+    
+    private Questionary currentQuestionary;
+    private Question currentQuestion;
+    private Iterator<Question> iteratorQuestion; 
 
     @Override
     protected EntityManager getEntityManager() {
@@ -26,7 +38,29 @@ public class QuestionaryService extends AbstractService<Questionary> implements 
     }
 
     public QuestionaryService() {
-        super(Questionary.class);
+        super(Questionary.class);        
+    }
+    
+    @WebMethod
+    public String start(){
+        currentQuestionary = find(1);
+        iteratorQuestion = currentQuestionary.getQuestionCollection().iterator();
+        try {
+            while(nextQuestion()){
+                System.out.println("questão: " + currentQuestion.getQuestionContent());
+                Thread.sleep(10);
+            }
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(QuestionaryService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "done";
+    }
+    public boolean nextQuestion(){
+        if(!iteratorQuestion.hasNext())
+            return false;
+        currentQuestion = iteratorQuestion.next();
+        return true;
     }
     
 }
